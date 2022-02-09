95a2fa35d6e1b922912d12b51ee56e2d5bdad2ae
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-02-09 10:39:24 +0100

    created initial layout after wireframes

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>CityPop</Text>
+      <View style={styles.btnView}>
+        <Button 
+          title="SEARCH BY CITY" 
+          style={styles.btn} 
+          color="#B48EAD" 
+          onPress={() => console.log("1")} 
+          accessibilityLabel="Seacrh by city name"
+        />
+        <Button 
+          title="SEARCH BY COUNTRY" 
+          style={styles.btn} 
+          color="#5E81AC" 
+          onPress={() => console.log("2")} 
+          accessibilityLabel="Search by country name"
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +29,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2E3440',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 32,
+    marginTop: 'auto',
+  },
+  btnView: {
+    marginTop: 'auto',
+    marginBottom: 'auto',
+  },
+  btn: {
+    backgroundColor: '#4C566A',
+    color: '#5E81AC',
+    marginTop: '10%',
+  }
 });

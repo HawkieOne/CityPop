2580eaadd60c244ffb7b6425dbab588605a894b0
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-02-12 15:35:38 +0100

    added atom with results for country search

---
+++ atoms/atoms.js
@@ -0,0 +1,16 @@
+import { atom } from 'recoil';
+
+export const searchTypeState = atom({
+  key: 'searchTypeState',
+  default: "city",
+});
+
+export const resultCityState = atom({
+  key: 'resultCityState',
+  default: null,
+});
+
+export const resultCountryState = atom({
+  key: 'resultCountryState',
+  default: null,
+});

347a688a11b0e5eeca6857731a76f8e85d0d16f8
Håkan <hali0151@student.umu.se>
2022-02-20 12:24:52 +0100

    Implemented error handling
    When the user search for a country or
    city which the API does not
    recognize the user is shown an error message. An error message is also
    shown if the API does not give a 200
    OK: The error message still needs
    visual improvements

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { SearchContext } from "../shared/contexts";
 import { StyleSheet, Text, View, Button, BackHandler, TextInput, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -8,6 +8,7 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 import { resultCityState, searchTypeState, resultCountryState } from '../atoms/atoms';
 import { useNavigation } from '@react-navigation/native';
 const { getCode } = require('country-list');
+import { Snackbar } from 'react-native-paper';
 
 export default function SearchScreen() {
 
@@ -16,6 +17,8 @@ export default function SearchScreen() {
   const searchType = useRecoilValue(searchTypeState);
   const [cityResults, setCityResults] = useRecoilState(resultCityState);
   const [countryResults, setCountryResults] = useRecoilState(resultCountryState);
+  const [visible, setVisible] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const [text, onChangeText] = React.useState("");
 
@@ -23,6 +26,14 @@ export default function SearchScreen() {
     if (searchType === 'city') {
       axios.get(`http://api.geonames.org/searchJSON?name_equals=${text}&username=weknowit&maxRows=1`)
       .then((response) => {
+        if (response.status !== "OK") {
+          showErrorMessage("The server have a bad answer");
+          return;
+        }
+        if (response.data.totalResultsCount === 0) {
+          showErrorMessage("The city was not found");
+          return;
+        }
         setCityResults(response.data.geonames[0]);
         navigation.push("CityResults");
       })
@@ -33,7 +44,15 @@ export default function SearchScreen() {
       const countryCode = getCode(text);
       axios.get(`http://api.geonames.org/searchJSON?q=${text}&country=${countryCode}&featureClass=P&orderby=population&username=weknowit&maxRows=10`)
       .then((response) => {
-        setCountryResults(response.data);
+        if (response.status !== "OK") {
+          showErrorMessage("The server have a bad answer");
+          return;
+        }
+        if (response.data.totalResultsCount === 0) {
+          showErrorMessage("The country was not found");
+          return;
+        }
+        setCityResults(response.data.geonames[0]);
         navigation.push("CountryResults");
       })
       .catch(error => {
@@ -41,6 +60,16 @@ export default function SearchScreen() {
       });
     }
   };
+
+  const showErrorMessage = (message) => {
+    setErrorMessage(message);
+    setVisible(true);
+  }
+
+  const onDismissSnackBar = () => {
+    setErrorMessage("");
+    setVisible(false);
+  }
 
   return (
     <View style={styles.screen}>
@@ -62,6 +91,8 @@ export default function SearchScreen() {
               <Icon name="search" size={30} style={styles.icon} />
           </TouchableOpacity>
         </View> 
+
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
 
     </View>
   )
@@ -101,4 +132,8 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 100,
   },
+  errorMessage: {
+    height: "20px",
+    color: "red",
+  }
 });

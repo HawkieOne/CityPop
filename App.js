42d6f624c6a236175083683c7a29c2b505848472
Håkan <hali0151@student.umu.se>
2022-02-21 21:44:10 +0100

    Implemented better safe area
    The app worked in a emulator but not
    on an physical device. Therefore a
    safe area was added. This needs
    a wrapper around the whole app.

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import CityResults from './components/CityResults';
 import CountryResults from './components/CountryResults';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 export default function App() {
@@ -16,17 +17,19 @@ export default function App() {
     * It is used to transfer data between components in the app.
     */
     <RecoilRoot>
-      <NavigationContainer>
-        <Stack.Navigator 
-          initialRouteName="Home" 
-          screenOptions={{headerShown: false}}
-        >
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="SearchScreen" component={SearchScreen} />
-          <Stack.Screen name="CityResults" component={CityResults} />
-          <Stack.Screen name="CountryResults" component={CountryResults} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator 
+            initialRouteName="Home" 
+            screenOptions={{headerShown: false}}
+          >
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="SearchScreen" component={SearchScreen} />
+            <Stack.Screen name="CityResults" component={CityResults} />
+            <Stack.Screen name="CountryResults" component={CountryResults} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </RecoilRoot>
   );
 }
